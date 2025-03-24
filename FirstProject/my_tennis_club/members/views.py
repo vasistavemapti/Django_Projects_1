cee53f51f7103a758a0fb58c39421fbdb6cbb7cb
Vasista_Vempati <vasista45@gmail.com>
2025-03-24 20:57:00 +0530

    new code

--- FirstProject/my_tennis_club/members/views.py
+++ FirstProject/my_tennis_club/members/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Member
 
 def members(request):
-    return HttpResponse("Hello world!")
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
